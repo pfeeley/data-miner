a99e0323e3e61ddd821e931e132ba70ca1d65294
admin <admin@localhost.localdomain>
2013-10-03 09:12:30 -0400

    added db links to pull from config

--- dbConnect.rb
+++ dbConnect.rb
@@ -4,6 +4,10 @@ require 'yaml'
 
 CONFIG = YAML.load_file("./config.yml")
 
-mysql = Mysql.new('localhost', 'patridx2_miner', CONFIG["password"], 'patridx2_mining')
+#TODO: update all connection details to pull data from config file
+#localhost,patridx2_miner,*,patridx2_mining
+
+
+mysql = Mysql.new(CONFIG["hostname"], CONFIG["username"], CONFIG["password"], CONFIG["database"])
 mysql.query('select DATABASE()')
 mysql.close;
