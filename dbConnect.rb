d28d6763d4befda78b4fd67926c22b6fe4cce929
pfeeley <patridx2@box720.bluehost.com>
2013-10-03 09:03:59 -0600

    update config file to include all of hte database connection fields

--- dbConnect.rb
+++ dbConnect.rb
@@ -4,10 +4,6 @@ require 'yaml'
 
 CONFIG = YAML.load_file("./config.yml")
 
-#TODO: update all connection details to pull data from config file
-#localhost,patridx2_miner,*,patridx2_mining
-
-
 mysql = Mysql.new(CONFIG["hostname"], CONFIG["username"], CONFIG["password"], CONFIG["database"])
 mysql.query('select DATABASE()')
 mysql.close;
