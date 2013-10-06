d020eaceae6b1de735f051ad6aa2dd3f10d448bc
pfeeley <patridx2@box720.bluehost.com>
2013-10-06 07:41:55 -0600

    moved pulling reviews to function

--- getReviews.rb
+++ getReviews.rb
@@ -8,35 +8,31 @@ require 'yaml'
 CONFIG = YAML.load_file("./config.yml")
 
 #TODO: pull information from database and generate URL automatically
+@db = Mysql.new(CONFIG['hostname'],CONFIG['username'],CONFIG['password'],CONFIG['database'])
 
-name = "Motorola-Droid-X-8GB-Black-Verizon-Smartphone-"
-file = Nokogiri::HTML(open("http://www.ebay.com/rvw/#{name}/102650543?_pgn=1"))
+@name = "Motorola-Droid-X-8GB-Black-Verizon-Smartphone-"
+@iid = "102650543"
+
+file = Nokogiri::HTML(open("http://www.ebay.com/rvw/#{@name}/#{@iid}?_pgn=1"))
 pages = Integer(file.xpath("//div[@class='rvp-pgn']").text.split('\n')[0].split(' ')[3].delete "^0-9")
-puts "Getting content for page 1"
-reviews = file.xpath("//div[@id='Reviews']").first
 
-#TODO: refactor single vs multiple reviews
-
-begin
-  db = Mysql.new(CONFIG['hostname'],CONFIG['username'],CONFIG['password'],CONFIG['database'])
-
+#This method take the number of pages of reviews and pulls their review content
+def pullReview(pages)
   if pages > 1
-    for i in 2..pages
-      new_review = db.prepare "insert into reviews (page, review_hash) values (?,?)"
+    for i in 1..pages
+      new_review = @db.prepare "insert into reviews (page, review_hash) values (?,?)"
       puts "Getting content for page #{i}\n"
-      doc = Nokogiri::HTML(open("http://www.ebay.com/rvw/#{name}/102650543?_pgn=#{i}"))
+      doc = Nokogiri::HTML(open("http://www.ebay.com/rvw/#{@name}/#{@iid}?_pgn=#{i}"))
       reviews = doc.xpath("//div[@id='Reviews']").first
       hash = Digest::SHA1.hexdigest reviews
       puts hash
-      new_review.execute i, hash
+      #new_review.execute i, hash
       new_review.close
     end
   end
-
-ensure
-  db.close
 end
 
+pullReview(pages)
 
 #TODO: extract review data from pulled content
 #TODO: tokenize reviews and save to database index
