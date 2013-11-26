5c4ec3d60fde99b6598449858984464a208dc10b
pfeeley <patridx2@box720.bluehost.com>
2013-11-26 14:21:18 -0700

    update parse to extract star trating for review

--- parseReviews.rb
+++ parseReviews.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 def parseReviews(star_rating, review_body)
   review_body.split(" ")
   for review_body.each do |word|
+    puts word
     #TODO: check if the word already exists in the index, add if new
     #TODO: cycle through words and calculate score
   end
--- stripReviews.rb
+++ stripReviews.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'mysql'
 require 'nokogiri'
 require 'yaml'
+#require 'parseReviews'
 
 CONFIG = YAML.load_file("./config.yml")
 db = Mysql.new(CONFIG['hostname'],CONFIG['username'],CONFIG['password'],CONFIG['database'])
@@ -14,6 +15,9 @@ puts "stopwords loaded..."
 reviews = db.query("select * from reviews where parsed != 'Y'")
 reviews.each_hash do |review|
   review_html = Nokogiri::HTML(review["review_text"])
+  review_rating = review_html.xpath("//span[@itemprop='reviewRating']/@content")
   review_body = review_html.xpath("//div[@itemprop='reviewBody']")
-  puts review_body
+  puts review_rating
+  #parseReviews()
+  #db.execute("") #update parsed reviews flag
 end
