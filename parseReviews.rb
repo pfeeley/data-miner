8caf2f7de218668b62bfd0534a413a69a4383481
pfeeley <patridx2@box720.bluehost.com>
2013-11-26 15:16:22 -0700

    loops through words in string

--- parseReviews.rb
+++ parseReviews.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 
 def parseReviews(star_rating, review_body)
-  review_body.split(" ")
-  for review_body.each do |word|
+  review_body = review_body.to_s.split(" ")
+  review_body.each do |word|
     puts word
     #TODO: check if the word already exists in the index, add if new
     #TODO: cycle through words and calculate score
