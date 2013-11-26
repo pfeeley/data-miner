b92fab335e73dce79fc7fabf860b7c99749ced20
pfeeley <patridx2@box720.bluehost.com>
2013-11-26 14:57:16 -0700

    added include for parse function

--- stripReviews.rb
+++ stripReviews.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'mysql'
 require 'nokogiri'
 require 'yaml'
-#require 'parseReviews'
+require './parseReviews'
 
 CONFIG = YAML.load_file("./config.yml")
 db = Mysql.new(CONFIG['hostname'],CONFIG['username'],CONFIG['password'],CONFIG['database'])
@@ -17,7 +17,6 @@ reviews.each_hash do |review|
   review_html = Nokogiri::HTML(review["review_text"])
   review_rating = review_html.xpath("//span[@itemprop='reviewRating']/@content")
   review_body = review_html.xpath("//div[@itemprop='reviewBody']")
-  puts review_rating
-  #parseReviews()
+  #parseReviews(review_rating, review_body)
   #db.execute("") #update parsed reviews flag
 end
