f79d46086a9fc12421b5b82aa1673ae9d06bd914
pfeeley <patridx2@box720.bluehost.com>
2013-11-25 20:02:48 -0700

    updated stripreviews to only pull review body

--- stripReviews.rb
+++ stripReviews.rb
@@ -5,5 +5,10 @@ require 'yaml'
 
 CONFIG = YAML.load_file("./config.yml")
 db = Mysql.new(CONFIG['hostname'],CONFIG['username'],CONFIG['password'],CONFIG['database'])
-
-
+stop_words = []
+reviews = db.query("select * from reviews")
+reviews.each_hash do |review|
+  review_html = Nokogiri::HTML(review["review_text"])
+  review_body = review_html.xpath("//div[@itemprop='reviewBody']").slice()
+  puts review_body
+end
