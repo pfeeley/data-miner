72d1045d1dca312653212396724d75eaae67b027
pfeeley <patridx2@box720.bluehost.com>
2013-11-25 20:15:37 -0700

    load stopwords for parsing

--- stripReviews.rb
+++ stripReviews.rb
@@ -5,10 +5,15 @@ require 'yaml'
 
 CONFIG = YAML.load_file("./config.yml")
 db = Mysql.new(CONFIG['hostname'],CONFIG['username'],CONFIG['password'],CONFIG['database'])
-stop_words = []
-reviews = db.query("select * from reviews")
+
+#load stopword list into memory
+stop_words = IO.readlines("./stopwords.txt")
+puts "stopwords loaded..."
+
+#load all existing reviews that have not been parsed yet 
+reviews = db.query("select * from reviews where parsed != 'Y'")
 reviews.each_hash do |review|
   review_html = Nokogiri::HTML(review["review_text"])
-  review_body = review_html.xpath("//div[@itemprop='reviewBody']").slice()
+  review_body = review_html.xpath("//div[@itemprop='reviewBody']")
   puts review_body
 end
