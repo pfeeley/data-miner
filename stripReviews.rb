56482813af6a1cecdbad06f4f6558528975a11a6
pfeeley <patridx2@box720.bluehost.com>
2013-11-26 19:27:28 -0700

    added stop words to regex

--- stripReviews.rb
+++ stripReviews.rb
@@ -16,8 +16,7 @@ reviews = db.query("select * from reviews where parsed != 'Y'")
 reviews.each_hash do |review|
   review_html = Nokogiri::HTML(review["review_text"])
   review_rating = review_html.xpath("//span[@itemprop='reviewRating']/@content")
-  review_body = review_html.xpath("//div[@itemprop='reviewBody']").to_s.gsub(/(<.*?>|[^a-z ])/i,"")
-  puts review_body
+  review_body = review_html.xpath("//div[@itemprop='reviewBody']").to_s.downcase.gsub(/(<.*?>|[^a-z ]|\b{1}#{stop_words.map{|x| x.chomp}.join("\\b|\\b")}\b)/i,"")
   parseReviews(review_rating, review_body)
   #db.execute("") #update parsed reviews flag
 end
