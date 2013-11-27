0b02d2be418520103e45a958188a49befbe7c2fe
pfeeley <patridx2@box720.bluehost.com>
2013-11-26 19:37:48 -0700

    added output for parsing

--- stripReviews.rb
+++ stripReviews.rb
@@ -14,9 +14,9 @@ puts "stopwords loaded..."
 #load all existing reviews that have not been parsed yet 
 reviews = db.query("select * from reviews where parsed != 'Y'")
 reviews.each_hash do |review|
+  puts "processing item #{review["item"]} review #{review["review_hash"]}"
   review_html = Nokogiri::HTML(review["review_text"])
   review_rating = review_html.xpath("//span[@itemprop='reviewRating']/@content")
   review_body = review_html.xpath("//div[@itemprop='reviewBody']").to_s.downcase.gsub(/(<.*?>|[^a-z ]|\b{1}#{stop_words.map{|x| x.chomp}.join("\\b|\\b")}\b)/i,"")
   parseReviews(review_rating, review_body)
-  #db.execute("") #update parsed reviews flag
 end
