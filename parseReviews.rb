19ade18d0d99684443ead178b6ef1feb335bb055
pfeeley <patridx2@box720.bluehost.com>
2013-11-27 07:21:55 -0700

    updated db interactions to use prepared statements

--- parseReviews.rb
+++ parseReviews.rb
@@ -1,22 +1,28 @@
 require 'rubygems'
 require 'mysql'
 
-CONFIG = YAML.load_file("./config.yml")
-
 def parseReviews(star_rating, review_body, item, hash)
   db = Mysql.new(CONFIG['hostname'],CONFIG['username'],CONFIG['password'],CONFIG['database'])
 
   review_body = review_body.to_s.split(" ")
+
+  #prepare database operations
+  records_query = db.prepare("select * from `word_index` where word = ?")
+  new_word      = db.prepare("insert into `word_index` (word, count, score) values (?,1,?)")
+  update_word   = db.prepare("update `word_index` set count = ?, score = ? where word = ?")
+
+  #parse each word in the review
   review_body.each do |word|
-    records = db.query("select * from `word_index` where word = '#{word}'")
+    records = records_query.execute(word)
     if records.num_rows == 0
-      db.query("insert into `word_index` (word, count, score) values ('#{word}',1,#{star_rating})")
+      new_word.execute word, star_rating
     else
       records.each_hash do |record|
         new_count = record["count"].to_f + 1.0
-        db.query("update `word_index` set count = #{new_count}, score = #{((record["count"].to_f*record["score"].to_f)+star_rating.to_s.to_f)/(new_count)} where word = '#{word}'")
+        new_score = ((record["count"].to_f*record["score"].to_f)+star_rating.to_s.to_f)/(new_count)
+        update_word.execute(new_count, new_score, word)
       end
-    end 
+    end
   end
   db.close
 end
--- stripReviews.rb
+++ stripReviews.rb
@@ -11,15 +11,20 @@ db = Mysql.new(CONFIG['hostname'],CONFIG['username'],CONFIG['password'],CONFIG['
 stop_words = IO.readlines("./stopwords.txt")
 puts "stopwords loaded..."
 
+#prepare update query
+update_parsed = db.prepare("update reviews set parsed = 'Y' where item = ? and review_hash = ?")
+
 #load all existing reviews that have not been parsed yet 
 reviews = db.query("select * from reviews where parsed != 'Y'")
+
+#parse the reviews
 reviews.each_hash do |review|
   puts "processing item #{review["item"]} review #{review["review_hash"]}"
   review_html = Nokogiri::HTML(review["review_text"])
   review_rating = review_html.xpath("//span[@itemprop='reviewRating']/@content")
   review_body = review_html.xpath("//div[@itemprop='reviewBody']").to_s.downcase.gsub(/(<.*?>|[^a-z ]|\b{1}#{stop_words.map{|x| x.chomp}.join("\\b|\\b")}\b)/i,"")
   parseReviews(review_rating, review_body, review["item"], review["review_hash"])
-  db.query("update reviews set parsed = 'Y' where item = #{review["item"]} and review_hash = '#{review["review_hash"]}'")
+  update_parsed.execute(review["item"], review["review_hash"])
 end
 
 db.close if db
