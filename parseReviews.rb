d42cf2b58a5cd1a7db5748fa15b64e5f15c16599
pfeeley <patridx2@box720.bluehost.com>
2013-11-28 09:03:30 -0700

    updated to handle new words for the index correctly

--- parseReviews.rb
+++ parseReviews.rb
@@ -8,14 +8,14 @@ def parseReviews(star_rating, review_body, item, hash)
 
   #prepare database operations
   records_query = db.prepare("select * from `word_index` where word = ?")
-  new_word      = db.prepare("insert into `word_index` (word, count, score) values (?,1,?)")
+  new_word = db.prepare("insert into `word_index` (`word`, `count`, `score`) values (?,1,?)")
   update_word   = db.prepare("update `word_index` set count = ?, score = ? where word = ?")
 
   #parse each word in the review
   review_body.each do |word|
     record = records_query.execute(word).fetch
-    if record.empty?
-      new_word.execute word, star_rating
+    if record.nil?
+      new_word.execute(word, star_rating.to_s.to_f)
     else
       new_count = record[1].to_f + 1.to_f
       new_score = ((record[1].to_f*record[2].to_f)+star_rating.to_s.to_f)/(new_count)
